010b71eb0923b7428a2a0f1a9587c709f42679c2
KhachikHarutyunyan <khachik789@gmail.com>
2024-11-06 16:26:43 +0400

    result1

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import uvicorn
 from fastapi import FastAPI
 
@@ -6,7 +8,8 @@ app = FastAPI(title="CI/CD Intro")
 
 @app.get("/")
 async def greating():
-    return {"result": "Hello from APP"}
+    env = (os.getenv("RUNNER_PATH"))
+    return {"result": f"Hello from APP:::{env}"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", workers=2, port=80, host="0.0.0.0")
