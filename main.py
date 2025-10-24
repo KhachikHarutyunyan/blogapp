bce2bf4e4935bcd8f2e330daea0d34fc30422701
KhachikHarutyunyan <khachik789@gmail.com>
2025-10-24 17:32:40 +0400

    add healthcheck

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ app = FastAPI(title="CI/CD Intro")
 @app.get("/")
 async def greating():
     env = (os.getenv("RUNNER_PATH"))
-    return {"result": f"Hello from APP:::{env}"}
+    return {"detail": f"Hello from APP:::{env}"}
+
+@app.get("/health")
+async def health():
+    return {"detail": "Healthy"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", workers=2, port=80, host="0.0.0.0")
